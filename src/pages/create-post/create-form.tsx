a558869117ac89adcb688ef930d00f3afac8e3b8
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-26 02:22:30 -0500

    Updated create-form
    Imported react-hook-forms, yup, and @hookforms/resolvers. Added form, inputs, and text areas. Created schema for title and description with validation from yup.

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,3 +1,21 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export const CreateForm = () => {
-   return <div>Form</div>;
+   const schema = yup.object().shape({
+      title: yup.string().required("You must add a title."),
+      description: yup.string().required("You must add a description"),
+   });
+
+   const {} = useForm({
+      resolver: yupResolver(schema),
+   });
+   return (
+      <form action="">
+         <input type="text" placeholder="Title..." />
+         <textarea placeholder="Description..." />
+         <input type="submit" />
+      </form>
+   );
 };
