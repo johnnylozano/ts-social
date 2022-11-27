76fc97f64bc1fdcf432f8d9c425e3c2db9ea6ec8
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-11-26 19:48:56 -0500

    Implemented post to database on create-form, styled create-form and create-post, implemented redirect to home page when submitting post

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,10 @@
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { addDoc, collection } from 'firebase/firestore';
+import { auth, db } from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 interface CreateFormData {
    title: string;
@@ -8,6 +12,9 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
+   const [user] = useAuthState(auth);
+   const navigate = useNavigate();
+
    const schema = yup.object().shape({
       title: yup.string().required('You must add a title.'),
       description: yup.string().required('You must add a description.'),
@@ -21,8 +28,16 @@ export const CreateForm = () => {
       resolver: yupResolver(schema),
    });
 
-   const onCreatePost = (data: CreateFormData) => {
-      console.log(data);
+   const postsRef = collection(db, 'posts');
+
+   const onCreatePost = async (data: CreateFormData) => {
+      await addDoc(postsRef, {
+         ...data,
+         username: user?.displayName,
+         userId: user?.uid,
+      });
+
+      navigate('/');
    };
 
    return (
@@ -31,7 +46,7 @@ export const CreateForm = () => {
          <p style={{ color: 'red' }}>{errors.title?.message}</p>
          <textarea placeholder="Description..." {...register('description')} />
          <p style={{ color: 'red' }}>{errors.description?.message}</p>
-         <input type="submit" />
+         <input type="submit" className="submitForm" />
       </form>
    );
 };
